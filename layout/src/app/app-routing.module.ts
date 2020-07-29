020dca997d3001b1fca4adc61d0c2f5b872af8ea
kabilangr <g.r.kabilan@gmail.com>
2020-07-29 14:51:28 +0530

    Finished

---
+++ layout/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'aboutus', component: AboutusComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
